866a30ff8c1065900d4f386e85ed98cec63a6af8
Sheeraz Ali <sheerazali656@gmail.com>
2020-07-02 08:30:09 -0700

    Initial Commit

---
+++ Crud_With_Image/Crud_With_Image/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Crud_With_Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.dataGridViewShowData = new System.Windows.Forms.DataGridView();
+            this.buttonBrowseImage = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDisplayData = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Crud With Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Id:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(104, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Age:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(169, 68);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(246, 20);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(169, 102);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(246, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(169, 140);
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(246, 20);
+            this.numericUpDownAge.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Image:";
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxImage.Image")));
+            this.pictureBoxImage.Location = new System.Drawing.Point(169, 184);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(246, 153);
+            this.pictureBoxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImage.TabIndex = 4;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // dataGridViewShowData
+            // 
+            this.dataGridViewShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewShowData.Location = new System.Drawing.Point(12, 407);
+            this.dataGridViewShowData.Name = "dataGridViewShowData";
+            this.dataGridViewShowData.Size = new System.Drawing.Size(607, 165);
+            this.dataGridViewShowData.TabIndex = 4;
+            this.dataGridViewShowData.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewShowData_MouseDoubleClick);
+            // 
+            // buttonBrowseImage
+            // 
+            this.buttonBrowseImage.Location = new System.Drawing.Point(218, 355);
+            this.buttonBrowseImage.Name = "buttonBrowseImage";
+            this.buttonBrowseImage.Size = new System.Drawing.Size(154, 34);
+            this.buttonBrowseImage.TabIndex = 3;
+            this.buttonBrowseImage.Text = "Browse Image";
+            this.buttonBrowseImage.UseVisualStyleBackColor = true;
+            this.buttonBrowseImage.Click += new System.EventHandler(this.buttonBrowseImage_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(453, 63);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(154, 34);
+            this.buttonInsert.TabIndex = 5;
+            this.buttonInsert.Text = "Insert Data";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDisplayData
+            // 
+            this.buttonDisplayData.Location = new System.Drawing.Point(453, 117);
+            this.buttonDisplayData.Name = "buttonDisplayData";
+            this.buttonDisplayData.Size = new System.Drawing.Size(154, 34);
+            this.buttonDisplayData.TabIndex = 6;
+            this.buttonDisplayData.Text = "Display Data";
+            this.buttonDisplayData.UseVisualStyleBackColor = true;
+            this.buttonDisplayData.Click += new System.EventHandler(this.buttonDisplayData_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(453, 175);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(154, 34);
+            this.buttonUpdate.TabIndex = 7;
+            this.buttonUpdate.Text = "Update Data";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(453, 231);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(154, 34);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete Data";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(453, 286);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(154, 34);
+            this.buttonReset.TabIndex = 9;
+            this.buttonReset.Text = "Reset Data";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 586);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDisplayData);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonBrowseImage);
+            this.Controls.Add(this.dataGridViewShowData);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.numericUpDownAge);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShowData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.DataGridView dataGridViewShowData;
+        private System.Windows.Forms.Button buttonBrowseImage;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDisplayData;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Crud_With_Image/Crud_With_Image/Form1.cs
@@ -0,0 +1,177 @@
+﻿using Crud_With_Image.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_With_Image
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowseImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select Image";
+            ofd.Filter = "ImageFile (*.png;*.jpg;*.bmp;*.gif;|*.png;*.jpg;*.bmp;*.gif)";
+            if (ofd.ShowDialog() == DialogResult.OK) {
+
+                pictureBoxImage.Image = new Bitmap(ofd.FileName);
+            }
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "insert into Student values (@id, @name, @age, @picture)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@age", numericUpDownAge.Value);
+            cmd.Parameters.AddWithValue("@picture", SavePhoto());
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Insertion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetControls();
+                BindGridView();
+            }
+            else {
+
+                MessageBox.Show("Insertion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+
+        }
+
+        private byte[] SavePhoto()
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBoxImage.Image.Save(ms, pictureBoxImage.Image.RawFormat);
+
+            return ms.GetBuffer();
+        }
+
+        public void BindGridView() {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Student";
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridViewShowData.DataSource = data;
+        }
+
+        private void buttonDisplayData_Click(object sender, EventArgs e)
+        {
+            BindGridView();
+        }
+
+        public void ResetControls() {
+
+            textBoxId.Clear();
+            textBoxName.Clear();
+            numericUpDownAge.Value = 0;
+            pictureBoxImage.Image = Resources.No_Image_Available;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ResetControls();
+        }
+
+        private void dataGridViewShowData_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxId.Text = dataGridViewShowData.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxName.Text = dataGridViewShowData.SelectedRows[0].Cells[1].Value.ToString();
+            numericUpDownAge.Value = Convert.ToInt32(dataGridViewShowData.SelectedRows[0].Cells[2].Value);
+
+            pictureBoxImage.Image = GetPhot((byte[])dataGridViewShowData.SelectedRows[0].Cells[3].Value);
+        }
+
+        private Image GetPhot(byte[] photo)
+        {
+            MemoryStream ms = new MemoryStream(photo);
+
+            return Image.FromStream(ms);
+            
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "update Student set Name = @name, Age = @age, Picture = @picture where Id = @id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@age", numericUpDownAge.Value);
+            cmd.Parameters.AddWithValue("@picture", SavePhoto());
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Updation Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetControls();
+                BindGridView();
+            }
+            else
+            {
+
+                MessageBox.Show("Updation Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "delete from Student where Id = @id";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Deletion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetControls();
+                BindGridView();
+            }
+            else
+            {
+
+                MessageBox.Show("Deletion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+        }
+    }
+}
